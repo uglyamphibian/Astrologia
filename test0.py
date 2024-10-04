a699f96c1a19830554fc04954f9f3e9bf7c3bb4f
b90303046 <b90303046@gmail.com>
2024-10-04 08:40:09 +0800

    補上test0.py

--- test0.py
+++ test0.py
@@ -14,22 +14,10 @@ d = {'年支':'火星'}
 
 # set_empty
 #%%
-sonus_list = list(sonus.keys())
 
 
-def empty_star(birth):
-    empty_dict={}
-    year_full = birth[0]
-    empty_loc = []
-    for ii in sonus_list:
-        list_tmp = [ii[0:2],ii[2:]]
-        empty_loc.extend(list_tmp)
-    np_empty = np.array(empty_loc).reshape(6,10)
-    empty_value = ['戌亥','申酉','午未','辰巳','寅卯','子丑']
-    v = np.where(np_empty==year_full)[0][0]
-    loc = empty_value[v]
-    empty_dict['旬中'], empty_dict['空亡'] = loc[0], loc[1]
-    return empty_dict
+
+
     
     
 
