33e646d82bd3d10acacbb47fde40018e0ca72a6d
b90303046 <b90303046@gmail.com>
2024-10-04 12:37:55 +0800

    test life example

--- test0.py
+++ test0.py
@@ -1,12 +1,22 @@
 #%%
+from tabulate import tabulate
 from functions import *
 from pprint import pprint
-life_dict2 = pd.read_excel('basic/example.xlsx',sheet_name='其他', index_col='姓名')
-life_example2 = []
-#pprint(life_dict2.keys())
+df = pd.read_excel('basic/example.xlsx',sheet_name='其他', index_col='姓名')
+life_dct2 = {ii: df.loc[ii,:].to_list() for ii in df.index}
+#pprint(life_dct2.keys())
+#print(life_dct2['李建佑'])
 
-#print(life_dict2['李建佑'])
-
-#print(combine_star(life_dict2['俞欣榮']))
+print(tabulate(combine_star(life_dct2['陳韻旻']),  tablefmt='psql'))
 
 
+#def flow_star():
+#    """
+    
+#    """
+#    pass
+
+
+# year_span
+# annual_span #小限 ## (要配合流年)
+# decade_span #大限 ## (要配合流年)
