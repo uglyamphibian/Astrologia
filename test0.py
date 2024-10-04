dca8acc5d6140f7a3cef0148fcbdd34849a0a4ef
b90303046 <b90303046@gmail.com>
2024-10-04 08:27:14 +0800

    補上旬中空亡排盤

--- test0.py
+++ test0.py
@@ -6,9 +6,7 @@ b = {'時':['文昌','文曲']}
 c ={'月':['左輔','右弼']}
 d = {'年支':'火星'}
 
-ex1 = life_dict['娼婦']
-print(ex1)
-aa = life_cycle(ex1, gender='F')
+
 #pprint(sonus)
 
 
@@ -18,19 +16,28 @@ aa = life_cycle(ex1, gender='F')
 #%%
 sonus_list = list(sonus.keys())
 
-empty_loc = []
-for ii in sonus_list:
-    list_tmp = [ii[0:2],ii[2:]]
-    empty_loc.extend(list_tmp)
 
-np_empty = np.array(empty_loc).reshape(6,10)
-empty_value = ['戌亥','申酉','午未','辰巳','寅卯','子丑']
-#VV = dict(zip(empty_value,np_empty))
+def empty_star(birth):
+    empty_dict={}
+    year_full = birth[0]
+    empty_loc = []
+    for ii in sonus_list:
+        list_tmp = [ii[0:2],ii[2:]]
+        empty_loc.extend(list_tmp)
+    np_empty = np.array(empty_loc).reshape(6,10)
+    empty_value = ['戌亥','申酉','午未','辰巳','寅卯','子丑']
+    v = np.where(np_empty==year_full)[0][0]
+    loc = empty_value[v]
+    empty_dict['旬中'], empty_dict['空亡'] = loc[0], loc[1]
+    return empty_dict
+    
+    
 
-v = np.where(np_empty=='壬子')[0][0]
-print(v)
+ex1 = life_dict['楊貴妃']
+print(ex1)
+aa = empty_star(ex1)
+print(aa)
+#life_cycle(ex1, gender='F')
 
 
-#def set_empty(birth):
-#    empty_dct = {}
 
